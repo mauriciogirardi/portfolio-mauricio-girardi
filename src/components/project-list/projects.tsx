d3690d15b9cc460ea71cd41aec793b22630348a2
Mauricio Girardi <maurigirarde@yahoo.com.br>
2024-05-08 23:22:08 +0100

    adjust image project

--- src/components/project-list/projects.tsx
+++ src/components/project-list/projects.tsx
@@ -36,45 +36,48 @@ export function Projects() {
             key={project.id}
             className="custom-bg w-full space-y-4 overflow-hidden rounded-lg p-4 md:max-w-[1000px] md:p-6"
           >
-            <div className="flex flex-col gap-6 lg:flex-row">
+            <div className="flex flex-col items-start gap-6 lg:flex-row">
               <Image
                 src={project.img}
                 alt={project.name}
                 width={500}
                 height={300}
-                className="h-auto w-full rounded-lg object-cover lg:w-1/2"
+                className="h-auto w-full rounded-lg object-contain lg:w-1/2"
                 priority
               />
 
-              <div className="space-y-3">
+              <div className="flex flex-col justify-between gap-3">
                 <h1 className="text-lg font-medium text-zinc-300 md:text-2xl">
                   {project.name}
                 </h1>
-                <p className="text-pretty text-sm md:text-base">
-                  {project.description}
-                </p>
 
-                <div className="flex items-center justify-end gap-4 pt-9">
-                  <Link
-                    href={project.linkGithub}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex items-center gap-2 rounded-full border border-zinc-400 px-3 py-2 text-sm font-medium hover:bg-zinc-400 hover:text-zinc-900"
-                  >
-                    <Github className="size-4" />
-                    Github
-                  </Link>
-                  {project.demoLink && (
+                <div>
+                  <p className="text-pretty text-sm md:text-base">
+                    {project.description}
+                  </p>
+
+                  <div className="flex items-center justify-end gap-4 pt-9">
                     <Link
-                      href={project.demoLink}
+                      href={project.linkGithub}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="flex items-center gap-2 rounded-full bg-zinc-600 px-3 py-2 text-sm font-medium hover:bg-zinc-700"
+                      className="flex items-center gap-2 rounded-full border border-zinc-400 px-3 py-2 text-sm font-medium hover:bg-zinc-400 hover:text-zinc-900"
                     >
-                      <Globe className="size-4" />
-                      Demo
+                      <Github className="size-4" />
+                      Github
                     </Link>
-                  )}
+                    {project.demoLink && (
+                      <Link
+                        href={project.demoLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center gap-2 rounded-full bg-zinc-600 px-3 py-2 text-sm font-medium hover:bg-zinc-700"
+                      >
+                        <Globe className="size-4" />
+                        Demo
+                      </Link>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
