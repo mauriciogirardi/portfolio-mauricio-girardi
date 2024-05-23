5d4eef1ed08d36e7885561e0d24e934dbdce468a
Mauricio Girardi <maurigirarde@yahoo.com.br>
2024-05-23 22:39:26 +0100

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,22 +5,32 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://portfolio-mauricio-girardi.vercel.app/',
       changeFrequency: 'never',
-      priority: 1
+      priority: 1,
+      lastModified: '2024-05-23T21:27:31+00:00'
     },
     {
       url: 'https://portfolio-mauricio-girardi.vercel.app/about',
       changeFrequency: 'never',
-      priority: 0.8
+      priority: 0.8,
+      lastModified: '2024-05-23T21:27:31+00:00'
     },
     {
       url: 'https://portfolio-mauricio-girardi.vercel.app/projects',
-      changeFrequency: 'never',
-      priority: 0.7
+      changeFrequency: 'monthly',
+      priority: 0.7,
+      lastModified: '2024-05-23T21:27:31+00:00'
     },
     {
       url: 'https://portfolio-mauricio-girardi.vercel.app/contact',
       changeFrequency: 'never',
-      priority: 0.6
+      priority: 0.6,
+      lastModified: '2024-05-23T21:27:31+00:00'
+    },
+    {
+      url: 'https://portfolio-mauricio-girardi.vercel.app/resume-mauricio-girardi-2024.pdf',
+      changeFrequency: 'never',
+      priority: 0.8,
+      lastModified: '2024-05-23T21:27:31+00:00'
     }
   ]
 }
